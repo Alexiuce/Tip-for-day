c4f8752aeb389d6ce5e104c35f13db0c7d181931
Alexiuce <alexiuce@qq.com>
2017-06-20 10:04:16 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Tip-for-day
 a book for myself, write down for develop tip
+
+## Demo project All in one 
+### iOS 
+* AVFoundation 
+
+### Mac 
---
+++ Tip for Day Demo/AVFoundationDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AVFoundationDemo
+//
+//  Created by alexiuce  on 2017/6/20.
+//  Copyright © 2017年 com.Alexiuce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    var <#name#> = <#value#>
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
