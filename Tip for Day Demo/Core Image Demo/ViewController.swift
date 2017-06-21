1ceb60656bf4183194e1b6c72f672ceff982679d
Alexiuce <alexiuce@qq.com>
2017-06-21 09:46:56 +0800

    init CGImage

--- Tip for Day Demo/Core Image Demo/ViewController.swift
+++ Tip for Day Demo/Core Image Demo/ViewController.swift
@@ -22,3 +22,18 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController{
+    fileprivate func handleImage(_ targetImage : UIImage){
+    
+        guard let inputImage = targetImage.cgImage else {return}            // UIImage -> CGImage
+        let imgWidth = inputImage.width
+        let imgHeight = inputImage.height
+        let bytesPerPixel = 4                 // 每像素用4个字节(32位  表示 RGBA的颜色空间）
+        let bitPerCompent = 8              // RGBA中每个部分用 8 个bit
+        let bytesPerRow = bytesPerPixel * imgWidth   // 每行的字节数
+        let pixels = calloc(imgHeight * imgWidth, sizeof(uint))     // 申请内存空间，尺寸为图片的size，单个单元的大小为UInt（也就是32）位
+        
+        
+    }
+}
+
