8517d616c23ff3043fffe021e8c19508d60c60d9
Alexiuce <alexiuce@qq.com>
2017-06-23 14:01:00 +0800

    update BaiduMap

--- BaiduMap.md
+++ BaiduMap.md
@@ -15,14 +15,16 @@
   * 在项目中添加一个新Objective-C文件，会自动生成桥接文件，然后再将这个OC文件delete，这样就不需要再手动配置桥接文件路径
   * 应用的info.plist 中，需要设置**Bundle display name**，否则Baidu地图注册时报错
  
-* 应用
-*   
+* 显示地图
+   
    ```swift
     var _mapView : BMKMapView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
        _mapView = BMKMapView(frame: view.bounds)
+       // 设置地图显示放大的级别（3～21）： 值越大，放大越明显
+       _mapView?.zoomLevel = 18
        view.addSubview(_mapView!)
     }
 
@@ -35,8 +37,36 @@
         super.viewWillDisappear(animated)
         _mapView?.viewWillDisappear()
     }
-}
+    
    ``` 
+  
+* 定位
+
+  ```swift
+    // 定位服务对象
+        _locationService = BMKLocationService()
+         // 设置最小更新距离（Double类型）默认值为kCLDistanceFilterNone（位置发生任意改变都会更新）
+        _locationService.distanceFilter = 10;
+        // 设置定位代理
+        _locationService.delegate = self
+        // 开启定位
+        _locationService.startUserLocationService()
+        // 设置定位模式
+       _mapView?.userTrackingMode = BMKUserTrackingModeHeading
+       // 显示用户位置（小蓝点）:普通定位模式下，圆形小蓝点没有方向箭头
+       _mapView?.showsUserLocation = true
+        
+    // 在代理方法中，更新地图中心坐标    
+    func didUpdate(_ userLocation: BMKUserLocation!) {
+        // 更新地图数据：这里并不会刷新地图UI
+         _mapView?.updateLocationData(userLocation)
+        // 中心点坐标 ： 设置后，地图会显示坐标所在的城市地图
+        _mapView?.centerCoordinate = userLocation.location.coordinate  
+    }
+  ``` 
+   
+   
+   
 * 错误码参考
   [LBS控制服务返回码定义及常见问题汇总](http://bbs.lbsyun.baidu.com/forum.php?mod=viewthread&tid=42223&page=1&extra=#pid93042)
 
--- Tip for Day Demo/BaiduMap Demo/ViewController.swift
+++ Tip for Day Demo/BaiduMap Demo/ViewController.swift
@@ -9,23 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    var _locationService: BMKLocationService!
     var _mapView : BMKMapView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 定位服务对象
+        _locationService = BMKLocationService()
+        // 设置最小更新距离（Double类型）
+        _locationService.distanceFilter = 10;
+        // 设置定位代理
+        _locationService.delegate = self
+        // 开启定位
+        _locationService.startUserLocationService()
+        // 地图视图
        _mapView = BMKMapView(frame: view.bounds)
+        // 设置定位模式
+       _mapView?.userTrackingMode = BMKUserTrackingModeHeading
+        // 显示用户位置（小蓝点）:普通定位模式下，圆形小蓝点没有方向箭头
+       _mapView?.showsUserLocation = true
+        // 设置地图显示放大的级别（3～21）： 值越大，放大越明显
+       _mapView?.zoomLevel = 18
        view.addSubview(_mapView!)
     }
 
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         _mapView?.viewWillAppear()
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        _locationService.delegate = nil;
         _mapView?.viewWillDisappear()
     }
 }
+// MARK: BMKLocationServiceDelegate
 
+extension ViewController : BMKLocationServiceDelegate{
+  
+    func didUpdate(_ userLocation: BMKUserLocation!) {
+        print(userLocation.isUpdating ? "updating" : "no uping")
+        // 更新地图数据：这里并不会刷新地图UI
+         _mapView?.updateLocationData(userLocation)
+        // 中心点坐标 ： 设置后，地图会显示坐标所在的城市地图
+        _mapView?.centerCoordinate = userLocation.location.coordinate
+        // 设置地图显示的放大级数(3~21)
+       
+        
+    }
+}
