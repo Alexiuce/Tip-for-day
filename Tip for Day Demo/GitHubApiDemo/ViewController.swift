e5786c1d11b1673c982ea3f5254a5081e8408b2c
秀策 <xiuce@naitang.com>
2017-06-25 15:29:22 +0800

    github api demo

--- Tip for Day Demo/GitHubApiDemo/ViewController.swift
+++ Tip for Day Demo/GitHubApiDemo/ViewController.swift
@@ -15,25 +15,7 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let baseURL  = "https://api.github.com"
-        let appID = "240fdaed9235e27e6203"
-        let apiName = "/authorizations/clients/:" + appID
-        
-         let para =   ["client_secret": "2c363097e99fe86a0c813201077312d96cd7342d",
-            "scopes": ["public_repo"],
-            "note": "admin script"] as [String : Any]
-//        let para = ["client_secret" : "2c363097e99fe86a0c813201077312d96cd7342d"] as [String : Any]
-        Alamofire.request(baseURL + "/" + apiName, method: .put, parameters: para).responseJSON { (response) in
-            
-            guard let data = response.data else{
-                XCPrint(response.result.error?.localizedDescription)
-                return
-            }
-           let json = JSON.init(data: data)
-            XCPrint(json)
-            
-            
-        }
+       
 
         
     }
