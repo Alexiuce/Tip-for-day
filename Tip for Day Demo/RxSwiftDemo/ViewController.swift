a99cffd4883ec00118d1387f10b7dbee59876b13
Alexiuce <alexiuce@qq.com>
2017-06-29 11:33:23 +0800

    observable

--- Tip for Day Demo/RxSwiftDemo/ViewController.swift
+++ Tip for Day Demo/RxSwiftDemo/ViewController.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      example(of: "subscribe") { 
+        let one = 1
+        let two = 2
+        let three = 3
+        let observable = Observable.of(one,two,three)
+        _ = observable.subscribe(onNext: { (element) in
+            print(element)
+        })
     }
 
 }
 
+
+public func example(of description : String ,action:()->Void){
+    print("\n--- Example of: ",description,"---")
+    action()
+}
