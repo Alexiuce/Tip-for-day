dd67a7e33f6761cd69358c4fcf90db015f6a1f41
Alexiuce <alexiuce@qq.com>
2017-07-06 10:33:19 +0800

    realm&charts demo

--- Tip for Day Demo/ReamAndChartsDemo/ReamChartViewController.swift
+++ Tip for Day Demo/ReamAndChartsDemo/ReamChartViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Charts
+import RealmSwift
 
 class ReamChartViewController: UIViewController {
     
@@ -17,6 +18,7 @@ class ReamChartViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        removeAllData()
 
         // Do any additional setup after loading the view.
     }
@@ -27,7 +29,46 @@ class ReamChartViewController: UIViewController {
             v.count = (value as NSString).integerValue
             v.saveToDatabase()
             tfValue.text = ""
+            updateCharData()
         }
     }
+}
 
+extension ReamChartViewController{
+    fileprivate func updateCharData(){
+        // 从Realm数据库中获取数据
+        guard let visitorCount = getVisitorFromDatabase() else {return}
+        // 定义数组
+        var dataEntries : [BarChartDataEntry] = []
+        // 遍历从Realm中获取的结果
+        for i in 0 ..< visitorCount.count {
+            // 创建数据节点
+            let dataEntry = BarChartDataEntry(x: Double(i), y: Double(visitorCount[i].count))
+            // 添加到数组中
+            dataEntries.append(dataEntry)
+        }
+        // 创建数据集
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Visitor count")
+        // 
+        let chartData = BarChartData(dataSet: chartDataSet)
+        barChartView.data = chartData
+   
+    }
+    // 获取数据库中的结果
+    fileprivate func getVisitorFromDatabase() -> Results<VisitorCount>?{
+        
+        guard let realm = try? Realm() else {return nil}
+       
+        return realm.objects(VisitorCount.self)
+    }
+    // 删除数据库中的数据
+    fileprivate func removeAllData(){
+           guard let realm = try? Realm() else {return}
+    
+        // 从Realm数据库中获取数据
+        guard let visitorCount = getVisitorFromDatabase() else {return}
+       try? realm.write {
+            realm.delete(visitorCount)
+        }
+    }
 }
