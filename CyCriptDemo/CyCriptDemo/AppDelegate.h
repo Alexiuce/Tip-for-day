d13b956033f760a33ced249dc8b83ef6a163cda3
Alexiuce <alexiuce@qq.com>
2017-07-17 17:16:30 +0800

    add cycriptDemo

---
+++ CyCriptDemo/CyCriptDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CyCriptDemo
+//
+//  Created by alexiuce  on 2017/7/17.
+//  Copyright © 2017年 alexiuce . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CyCriptDemo/CyCriptDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CyCriptDemo
+//
+//  Created by alexiuce  on 2017/7/17.
+//  Copyright © 2017年 alexiuce . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
