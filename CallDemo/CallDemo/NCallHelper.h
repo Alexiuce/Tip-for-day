fba3450272ccf16ac4058c3a57ae3f5f591c5d21
Alexiuce <alexiuce@qq.com>
2017-07-21 10:39:35 +0800

    update call demo

---
+++ CallDemo/CallDemo/NCallHelper.h
@@ -0,0 +1,16 @@
+//
+//  NCallHelper.h
+//  CallDemo
+//
+//  Created by alexiuce  on 2017/7/21.
+//  Copyright © 2017年 alexiuce . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NCallHelper : NSObject
+
+
+
+
+@end
---
+++ CallDemo/CallDemo/NCallHelper.m
@@ -0,0 +1,14 @@
+//
+//  NCallHelper.m
+//  CallDemo
+//
+//  Created by alexiuce  on 2017/7/21.
+//  Copyright © 2017年 alexiuce . All rights reserved.
+//
+
+#import "NCallHelper.h"
+#import <ALCall_Sdk/ALCall_Sdk.h>
+
+@implementation NCallHelper
+
+@end
