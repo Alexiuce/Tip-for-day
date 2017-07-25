57e64220dbbb0cab6a16b1f13cdaeea798e1351e
Alexiuce <alexiuce@qq.com>
2017-07-25 08:59:53 +0800

    add timeAnywhere

---
+++ TimeAnywhere/TimeAnywhere/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TimeAnywhere
+//
+//  Created by alexiuce  on 2017/7/25.
+//  Copyright © 2017年 alexiuce . All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
