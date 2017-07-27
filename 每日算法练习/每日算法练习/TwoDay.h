3f8f9b684292be22fbcb2146f4d2b8cdb1d20510
Alexiuce <alexiuce@qq.com>
2017-07-27 09:06:21 +0800

    tow day caculate

---
+++ 每日算法练习/每日算法练习/TwoDay.h
@@ -0,0 +1,23 @@
+//
+//  TwoDay.h
+//  每日算法练习
+//
+//  Created by alexiuce  on 2017/7/27.
+//  Copyright © 2017年 alexiuce . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ 给你两个非空链表(链表不懂的自行百度,简单的说就是一个结构体,里面有数据和一个指针,指针指向下一个结构体,所以地址可以不连续),
+ 每个链表代表一个数,
+ 把这两个数加起来,返回结果.
+ 结果也是以链表的形式返回
+ (给的数据不会以0开头,唯一以 0 开头的情况是数字0本身,所以不用担心前面有多余0的情况).
+ 
+ */
+
+
+@interface TwoDay : NSObject
+
+@end
---
+++ 每日算法练习/每日算法练习/TwoDay.m
@@ -0,0 +1,13 @@
+//
+//  TwoDay.m
+//  每日算法练习
+//
+//  Created by alexiuce  on 2017/7/27.
+//  Copyright © 2017年 alexiuce . All rights reserved.
+//
+
+#import "TwoDay.h"
+
+@implementation TwoDay
+
+@end
