69922baf9e669ea2b3d866707d19aeb3b9b9813a
Alexiuce <alexiuce@qq.com>
2017-07-29 14:01:22 +0800

    add githelper

---
+++ Tip for Day Demo/GitHubApiDemo/GitHelper.swift
@@ -0,0 +1,20 @@
+//
+//  GitHelper.swift
+//  Tip for Day Demo
+//
+//  Created by Alexcai on 2017/7/29.
+//  Copyright © 2017年 com.Alexiuce. All rights reserved.
+//
+
+import Cocoa
+
+class GitHelper: NSObject {
+    
+    
+
+}
+
+extension GitHelper{
+    open func gitClone(_ repoUrl:String){
+    }
+}
