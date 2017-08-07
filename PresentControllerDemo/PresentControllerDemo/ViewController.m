a9402d6358b509a0d94b4229f6400f06b2476e08
Alexiuce <alexiuce@qq.com>
2017-08-07 18:01:45 +0800

    add comment for presentControllerDemo

--- PresentControllerDemo/PresentControllerDemo/ViewController.m
+++ PresentControllerDemo/PresentControllerDemo/ViewController.m
@@ -34,9 +34,23 @@
     /** 自定义方式弹控制器, 需提供一个实现<NSViewControllerPresentationAnimator> 协议的类,完成动画 */
 //    [self presentViewController:[[TwoController alloc]init] animator:<#(nonnull id<NSViewControllerPresentationAnimator>)#>];
     
-    /** 以 popover 方式弹出新的控制器 */
+    /** 以 popover 方式弹出新的控制器 
+     asPopoverRelativeToRect : 弹出popover时,附着的矩形区域(即被popover剪头指向的那一块矩形区域)
+     ofView: 矩形区域( asPopoverRelativeToRect)所属的view
+     preferredEdge:矩形区域( asPopoverRelativeToRect)的参考边界:
+                     NSRectEdgeMinX:(在矩形区域左边显示popover)
+                     NSRectEdgeMaxX:(在矩形区域右边显示popover)
+                     NSRectEdgeMinY:(在矩形区域顶部显示popover)
+                     NSRectEdgeMaxY:(在矩形区域底部显示popover)
+     behavior : popover的行为模式
+     
+     */
+    
+    
+    
+    
     TwoController *twoVc  = [[TwoController alloc]init];
-    [self presentViewController:twoVc asPopoverRelativeToRect:sender.frame ofView:self.view preferredEdge:NSRectEdgeMinX behavior:NSPopoverBehaviorTransient];
+    [self presentViewController:twoVc asPopoverRelativeToRect:sender.frame ofView:self.view preferredEdge:NSRectEdgeMinY behavior:NSPopoverBehaviorTransient];
     
 }
 
