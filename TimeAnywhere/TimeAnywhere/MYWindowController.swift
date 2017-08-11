097e81351607409ed0fbe6d5faa8c9000b3f7c0b
Alexiuce <alexiuce@qq.com>
2017-08-11 09:41:40 +0800

    timeAnywhere add GestureRecognize

--- TimeAnywhere/TimeAnywhere/MYWindowController.swift
+++ TimeAnywhere/TimeAnywhere/MYWindowController.swift
@@ -13,6 +13,8 @@ class MYWindowController: NSWindowController {
     override func windowDidLoad() {
         super.windowDidLoad()
         window?.level =  Int(CGShieldingWindowLevel())
+        
+        
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
 
--- TimeAnywhere/TimeAnywhere/ViewController.swift
+++ TimeAnywhere/TimeAnywhere/ViewController.swift
@@ -11,13 +11,24 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var imageView: NSImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        NSWorkspace.shared().notificationCenter.addObserver(self, selector: #selector(ViewController.updateWorkspace(_:)), name: .NSWorkspaceActiveSpaceDidChange, object: nil)
+        
+//        NSWorkspace.shared().notificationCenter.addObserver(self, selector: #selector(ViewController.updateWorkspace(_:)), name: .NSWorkspaceActiveSpaceDidChange, object: nil)
 //        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTopAppInfo), userInfo: nil, repeats: true)
        
         // Do any additional setup after loading the view.
+        
+        
+        /** 创建手势识别 */
+        let clickGesture = NSClickGestureRecognizer(target: self, action: #selector(clcikedMouse(_:)))
+        /** 将手势识别器添加到imageView */
+        imageView.addGestureRecognizer(clickGesture)
+        
+        
+        
     }
 
 }
@@ -35,6 +46,14 @@ extension ViewController{
    @objc fileprivate func updateTopAppInfo(){
     
       print(NSWorkspace.shared().frontmostApplication ?? "no no")
+    }
+}
+
+// MARK: handle event listion
+extension ViewController{
+    @objc fileprivate func clcikedMouse(_ gesuter: NSClickGestureRecognizer){
+        print("clicked mouse")
+        
     }
 }
 
