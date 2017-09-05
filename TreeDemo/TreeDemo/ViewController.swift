d55da7e8106e3e0c9870350894acdd2ee672dd26
Alexiuce <alexiuce@qq.com>
2017-09-05 13:11:03 +0800

    update TreeController selection

--- TreeDemo/TreeDemo/ViewController.swift
+++ TreeDemo/TreeDemo/ViewController.swift
@@ -10,8 +10,8 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var outline: NSOutlineView!
     
-    @IBOutlet var treeController: NSTreeController!
     
    lazy var nodes : [Node] = {
         let n1 = Node("One")
@@ -40,7 +40,17 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
 }
+
+// MARK: NSOutlineView Delegate Mothed
+
+extension ViewController : NSOutlineViewDelegate{
+    func outlineView(_ outlineView: NSOutlineView, shouldSelectItem item: Any) -> Bool {
+        // 1. item 为将要选择的数据模型对象
+        print("select item \(item.self)")
+        // 2. 这里可以处理用户选择时的业务逻辑，比如根据选中是数据模型进行相应的网络请求，
+        return true
+    }
+}
+
 
