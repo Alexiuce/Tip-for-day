ac98832791fe1150ad0fcc327e2c08e66b36f9d9
Alexiuce <alexiuce@qq.com>
2017-10-02 18:26:21 +0800

    present controller

---
+++ PresentControllerDemo/PresentControllerDemo/TwoView.h
@@ -0,0 +1,13 @@
+//
+//  TwoView.h
+//  PresentControllerDemo
+//
+//  Created by Alexcai on 2017/10/2.
+//  Copyright © 2017年 alexiuce . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TwoView : NSView
+
+@end
