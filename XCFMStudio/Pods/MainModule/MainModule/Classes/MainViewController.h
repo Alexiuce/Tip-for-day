49e9f9073b49765578540dd9af015ba8e556eb46
Alexiuce <alexiuce@qq.com>
2017-12-02 21:47:30 +0800

    add module

---
+++ XCFMStudio/Pods/MainModule/MainModule/Classes/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  MainModule_Example
+//
+//  Created by Alexcai on 2017/12/2.
+//  Copyright © 2017年 leiocai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ XCFMStudio/Pods/Target Support Files/MainModule/MainModule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MainModule : NSObject
+@end
+@implementation PodsDummy_MainModule
+@end
