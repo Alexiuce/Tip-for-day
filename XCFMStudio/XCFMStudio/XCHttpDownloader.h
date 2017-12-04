5b5fa6aef1822a9f54e7d17e3331e8d44ee7e6e0
Alexiuce <alexiuce@qq.com>
2017-12-04 13:49:01 +0800

    add XCHttpDownloader

---
+++ XCFMStudio/XCFMStudio/XCHttpDownloader.h
@@ -0,0 +1,17 @@
+//
+//  XCHttpDownloader.h
+//  XCFMStudio
+//
+//  Created by caijinzhu on 2017/12/4.
+//  Copyright © 2017年 caijinzhu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XCHttpDownloader : NSObject
+
+
+
+
+
+@end
---
+++ XCFMStudio/XCFMStudio/XCHttpDownloader.m
@@ -0,0 +1,13 @@
+//
+//  XCHttpDownloader.m
+//  XCFMStudio
+//
+//  Created by caijinzhu on 2017/12/4.
+//  Copyright © 2017年 caijinzhu. All rights reserved.
+//
+
+#import "XCHttpDownloader.h"
+
+@implementation XCHttpDownloader
+
+@end
