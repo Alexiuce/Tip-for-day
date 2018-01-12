b5f5ec2030772f7c7bedbb98f4cbd8b1d96a8c02
Alexiuce <alexiuce@qq.com>
2018-01-12 16:58:26 +0800

    update

--- Cocoa2Demo/GameScene.m
+++ Cocoa2Demo/GameScene.m
@@ -9,6 +9,7 @@
 #import "GameScene.h"
 #import "QQSprite.h"
 
+#import <CCActionManager.h>
 #import <CoreMotion/CoreMotion.h>
 
 
@@ -30,23 +31,32 @@ static const CGFloat kAnimationDuration = 30.0;
 - (id)init{
     self = [super init];
     NSAssert(self, @"game scene init failure");
-//    _motionManager = [[CMMotionManager alloc]init];
-     self.userInteractionEnabled = YES;
-    CCNodeColor *bg = [CCNodeColor nodeWithColor:CCColor.grayColor];
-    [self addChild:bg];
+    self.userInteractionEnabled = YES;
     
-    _qq = [CCSprite spriteWithImageNamed:@"qq.png"];
+    // 设置背景
+//    CCNodeColor *bg = [CCNodeColor nodeWithColor:CCColor.grayColor];
+//    [self addChild:bg];
     
-    _qq.position = ccp(100, 100);
-   
-
-    
-    [self addChild:_qq];
-    
-//    [self setupUI];
-    
-    
+    // 创建精灵
+    CCSprite *qq = [CCSprite spriteWithImageNamed:@"qq.png"];
+    qq.position = ccp(100, 100);
+    [self addChild:qq];
+    self.qq = qq;
+//    // 添加动画
+//    CCActionMoveTo *moveAction = [CCActionMoveTo actionWithDuration:2.0 position:ccp(300, 100)];
+//
+//    // 添加动画回调动作
+//    CCActionCallFunc *endAction = [CCActionCallFunc actionWithTarget:self selector:@selector(endMoveToAction)];
+//    // 创建动画执行队列
+//    CCActionSequence *sequence = [CCActionSequence actions:moveAction,endAction, nil];
+//    [qq runAction:sequence];
+    [self setupUI];
+//
     return self;
+}
+
+- (void)endMoveToAction{
+    CCLOG(@"动画执行完毕 ~~");
 }
 
 - (void)setupUI{
@@ -59,26 +69,15 @@ static const CGFloat kAnimationDuration = 30.0;
     CGFloat height = background1.contentSize.height - 1;
     background1.anchorPoint = CGPointZero;
     background1.position = ccp(0, height);
-    [self addChild:background1];
+    [self addChild:background1 z:-1];
     
     _background = [CCSprite spriteWithSpriteFrame:[self.spriteFrameCache spriteFrameByName:@"background_2.png"]];
     
     _background.anchorPoint = CGPointZero;
     
-    [self addChild:_background];
+    [self addChild:_background z:-1];
     
     // 添加文字
-    
-    
-    // 添加图片
-    CCSprite *qq = [CCSprite spriteWithImageNamed:@"qq.png"];
-//    qq.positionType = CCPositionTypeMake(CCPositionUnitUIPoints, CCPositionUnitNormalized, CCPositionReferenceCornerTopRight);
-    qq.position = ccp(100, 100);
-    [self addChild:qq];
-    
-   
-    
-    
     
     // 添加移动动画
     
@@ -94,7 +93,7 @@ static const CGFloat kAnimationDuration = 30.0;
     
     CCAction *action = [CCActionMoveBy actionWithDuration:kAnimationDuration position:ccp(0, -height)];
     CCAction *action1 = [CCActionMoveTo actionWithDuration:0 position:ccp(0, height)];
-    CCActionInterval *seqActon = [CCActionSequence actionWithArray:@[action,action1,action]];
+    CCActionSequence *seqActon = [CCActionSequence actionWithArray:@[action,action1,action]];
     CCActionRepeatForever *foreverAction = [CCActionRepeatForever actionWithAction: seqActon];
     [_background runAction:foreverAction];
 }
@@ -104,16 +103,15 @@ static const CGFloat kAnimationDuration = 30.0;
     
     [self.qq stopActionByTag:0];
     CGPoint p = [touch locationInNode:self];
+    // 获取屏幕size
+    CGSize screenSize = [CCDirector sharedDirector].viewSize;
+    // 根据两点之间的距离计算移动时间
+    CGFloat duration = ccpDistance(p, self.qq.position) / 200.0;
+    // 限制边界值
+    p.x = clampf(p.x, self.qq.contentSize.width / 2, screenSize.width - self.qq.contentSize.width / 2);
+    p.y = clampf(p.y, self.qq.contentSize.height / 2,screenSize.height - self.qq.contentSize.height / 2);
     
-    CCLOG(@"target p = %@",NSStringFromCGPoint(p));
-    CGFloat d = ccpDistance(p, self.qq.position) / 200.0;
-    
-    CCLOG(@"distance %.2f" ,d);
-    
-    p.x = clampf(p.x, _qq.contentSize.width / 2, 375 - _qq.contentSize.width / 2);
-    p.y = clampf(p.y, _qq.contentSize.height / 2, 667 - _qq.contentSize.height / 2);
-    
-    CCActionMoveTo *moveAction = [CCActionMoveTo actionWithDuration:d position:p];
+    CCActionMoveTo *moveAction = [CCActionMoveTo actionWithDuration:duration position:p];
     [moveAction setTag:0];
     [self.qq runAction:moveAction];
 }
