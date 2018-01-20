41b01e375144d9bcb25c7c81a539e7b3a954c048
Alexiuce <alexiuce@qq.com>
2018-01-20 17:24:42 +0800

    add OASimpleAudio

--- Cocoa2Demo/IADScene.m
+++ Cocoa2Demo/IADScene.m
@@ -13,12 +13,20 @@
 #import "TouchSprite.h"
 #import <CCTiledMap.h>
 #import <CCTiledMapLayer.h>
+#import "ObjectAL.h"
 
 static const int CountPerRow = 10;
 static const CGFloat MarginBetween = 5.0f;
 static const CGFloat BeginTopY = 64.0f;
 
 static int planeMap[10][10];
+
+@interface IADScene()
+
+
+@property (nonatomic, strong) NSArray<NSArray <NSNumber *>*> *map;
+
+@end
 
 @implementation IADScene
 
@@ -28,6 +36,7 @@ static int planeMap[10][10];
         CCNodeColor *bg = [CCNodeColor nodeWithColor:CCColor.grayColor];
         [self addChild:bg];
         [self loadTMX];
+        self.map = [self convertCArrayToNSArray];
         for (int i = 0; i < CountPerRow; i++) {
             [self addLine:i + 1];
         }
@@ -60,6 +69,27 @@ static int planeMap[10][10];
     }
 }
 
+- (NSArray<NSArray< NSNumber *>*>*)convertCArrayToNSArray{
+    NSMutableArray *allArray = [NSMutableArray arrayWithCapacity:CountPerRow];
+    for (int i = 0; i < CountPerRow; i++) {
+        NSMutableArray *rowArray = [NSMutableArray arrayWithCapacity:CountPerRow];
+        for (int j = 0; j < CountPerRow; j++) {
+            
+            [rowArray addObject:@(planeMap[i][j])];
+        }
+        [allArray addObject:rowArray];
+    }
+    printf("++++++++++++++++++++++++++++++++++");
+    for (int i = 0; i < CountPerRow; i++) {
+        for (int j = 0; j< CountPerRow; j++) {
+            int r = [allArray[i][j] intValue];
+             printf("%d",r);
+        }
+        printf("\n");
+    }
+    
+    return [allArray copy];
+}
 
 - (void)addLine:(NSUInteger)lineNumber{
     
@@ -71,24 +101,22 @@ static int planeMap[10][10];
     for (int i = 0; i < CountPerRow; i++) {
         CGFloat x = i * (spw + MarginBetween) + MarginBetween;
 //        RectSprite *r = [RectSprite spriteWithColor:CCColor.redColor size:CGSizeMake(spw, spw)];
-        int type = planeMap[lineNumber][i];
+        int type =  self.map[lineNumber - 1][i].intValue ; //planeMap[lineNumber][i];
         NSString *fn = @"yellow.png";
-        CCSpriteFrame *normalFrame = [CCSpriteFrame frameWithImageNamed:fn];
-        NSString *disableName = fn;
+//        CCSpriteFrame *normalFrame = [CCSpriteFrame frameWithImageNamed:fn];
+        NSString *disableName = @"empty.png";
         switch (type) {
-            case 5:
-                disableName = @"p1.png";
+            case 3:
+                disableName = @"head.png";
                 break;
             case 17:
-                disableName = @"p2.png";
-                break;
-            case 33:
-                disableName = @"p3.png";
+                disableName = @"body.png";
                 break;
         }
-        CCSpriteFrame *disableFrame = [CCSpriteFrame frameWithImageNamed:disableName];
-        CCButton *btn = [CCButton buttonWithTitle:@"" spriteFrame:normalFrame highlightedSpriteFrame:disableFrame disabledSpriteFrame:nil];
-//        CCButton *btn = [CCButton buttonWithTitle:@"" spriteFrame:[CCSpriteFrame frameWithImageNamed:fn]];
+//        CCSpriteFrame *disableFrame = [CCSpriteFrame frameWithImageNamed:disableName];
+//        CCButton *btn = [CCButton buttonWithTitle:@"" spriteFrame:normalFrame highlightedSpriteFrame:disableFrame disabledSpriteFrame:nil];
+        CCButton *btn = [CCButton buttonWithTitle:@"" spriteFrame:[CCSpriteFrame frameWithImageNamed:fn]];
+        btn.name = disableName;
         [btn setTarget:self selector:@selector(clickButton:)];
         [btn setScale:spw / btn.contentSize.width];
         btn.anchorPoint = CGPointZero;
@@ -104,10 +132,15 @@ static int planeMap[10][10];
     CCActionBlink *blink = [CCActionBlink actionWithDuration:0.3 blinks:2];
     [btn runAction:blink];
     
-    if (btn.state == CCControlStateDisabled) {
-        return;
-    }
-    btn.state = CCControlStateDisabled;
+    
+    CCSprite *s = [CCSprite spriteWithImageNamed:btn.name];
+    s.positionType = CCPositionTypeNormalized;
+    s.position = ccp(0.5, 0.5);
+    [btn addChild:s];
+    btn.userInteractionEnabled = NO;
+    
+    // 点击音效
+    [[OALSimpleAudio sharedInstance] playEffect:[btn.name stringByReplacingOccurrencesOfString:@"png" withString:@"mp3"]];
     
 }
 
