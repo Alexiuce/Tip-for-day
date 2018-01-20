eede34c642e06b0150bdab2fb70624ab0b2cafac
Alexiuce <alexiuce@qq.com>
2018-01-20 13:09:14 +0800

    setup CCButton states

--- Cocoa2Demo/IADScene.m
+++ Cocoa2Demo/IADScene.m
@@ -18,25 +18,20 @@ static const int CountPerRow = 10;
 static const CGFloat MarginBetween = 5.0f;
 static const CGFloat BeginTopY = 64.0f;
 
+static int planeMap[10][10];
+
 @implementation IADScene
 
 - (id)init{
     if (self = [super init]) {
-        
-        
         self.userInteractionEnabled = YES;
         CCNodeColor *bg = [CCNodeColor nodeWithColor:CCColor.grayColor];
         [self addChild:bg];
-        
-        
-        
-        
-      
+        [self loadTMX];
         for (int i = 0; i < CountPerRow; i++) {
             [self addLine:i + 1];
         }
         
-        [self loadTMX];
     }
     return self;
 }
@@ -49,23 +44,20 @@ static const CGFloat BeginTopY = 64.0f;
     [self addChild:map];
     
     CCTiledMapLayer *maplayer = [map layerNamed:@"bottom"];
-    XCLog(@"property : %@",maplayer);
-    
 
-    
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
               int begin = [maplayer tileGIDAt:ccp(j, i)];
             if (begin != 0 ) {
-                printf("1   ");
+                printf("%d  ",begin);
+                planeMap[i][j] = begin;
             }else{
                 printf("0   ");
+                planeMap[i][j] = 0;
             }
         }
         printf("\n");
     }
-    
-    
 }
 
 
@@ -79,19 +71,44 @@ static const CGFloat BeginTopY = 64.0f;
     for (int i = 0; i < CountPerRow; i++) {
         CGFloat x = i * (spw + MarginBetween) + MarginBetween;
 //        RectSprite *r = [RectSprite spriteWithColor:CCColor.redColor size:CGSizeMake(spw, spw)];
-        CCButton *btn = [CCButton buttonWithTitle:@"" spriteFrame:[CCSpriteFrame frameWithImageNamed:@"yellow.png"]];
-        btn.name = [NSString stringWithFormat:@"%zd",i];
+        int type = planeMap[lineNumber][i];
+        NSString *fn = @"yellow.png";
+        CCSpriteFrame *normalFrame = [CCSpriteFrame frameWithImageNamed:fn];
+        NSString *disableName = fn;
+        switch (type) {
+            case 5:
+                disableName = @"p1.png";
+                break;
+            case 17:
+                disableName = @"p2.png";
+                break;
+            case 33:
+                disableName = @"p3.png";
+                break;
+        }
+        CCSpriteFrame *disableFrame = [CCSpriteFrame frameWithImageNamed:disableName];
+        CCButton *btn = [CCButton buttonWithTitle:@"" spriteFrame:normalFrame highlightedSpriteFrame:disableFrame disabledSpriteFrame:nil];
+//        CCButton *btn = [CCButton buttonWithTitle:@"" spriteFrame:[CCSpriteFrame frameWithImageNamed:fn]];
         [btn setTarget:self selector:@selector(clickButton:)];
         [btn setScale:spw / btn.contentSize.width];
         btn.anchorPoint = CGPointZero;
         btn.position = ccp(x, beginY);
         [self addChild:btn];
+        
     }
 }
 
 #pragma mark - Button Target Event
 - (void)clickButton:(CCButton *)btn{
-    XCLog(@"%@",btn.name);
+   
+    CCActionBlink *blink = [CCActionBlink actionWithDuration:0.3 blinks:2];
+    [btn runAction:blink];
+    
+    if (btn.state == CCControlStateDisabled) {
+        return;
+    }
+    btn.state = CCControlStateDisabled;
+    
 }
 
 
