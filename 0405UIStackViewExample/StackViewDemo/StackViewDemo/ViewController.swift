22a2fe56e8ac5afc50a18ea0bdd7a040d32ab921
Alexiuce <alexiuce@qq.com>
2018-04-05 11:22:03 +0800

    add stackview example

---
+++ 0405UIStackViewExample/StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Alexcai on 2018/4/5.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
